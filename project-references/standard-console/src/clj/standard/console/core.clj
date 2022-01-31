7b0e8734022b8c81fe78c23e3edbb19d97587a03
zclj <info-s@tryne.se>
2022-01-31 08:12:02 +0100

    Add log and tap example

--- project-references/standard-console/src/clj/standard/console/core.clj
+++ project-references/standard-console/src/clj/standard/console/core.clj
@@ -1,5 +1,12 @@
-(ns standard.console.core)
+(ns standard.console.core
+  (:require [clojure.tools.logging :as log]))
 
 (defn hello
   [s]
   (str "Hello, " s))
+
+(defn do-log
+  []
+  (log/info "Logged")
+  (log/debug "Hi dev")
+  (tap> {:do-log {:interesting :stuff}}))
