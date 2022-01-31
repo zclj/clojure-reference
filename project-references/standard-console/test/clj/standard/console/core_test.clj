77a1a6844b86f419eb4dec8d864316d6b0e52ad0
zclj <info-s@tryne.se>
2022-01-31 08:12:02 +0100

    Fix tests

--- project-references/standard-console/test/clj/standard/console/core_test.clj
+++ project-references/standard-console/test/clj/standard/console/core_test.clj
@@ -1,6 +1,6 @@
 (ns standard.console.core-test
   (:require [standard.console.core :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [deftest is]]))
 
 (deftest should-say-hello
   (is (= "Hello, world"
