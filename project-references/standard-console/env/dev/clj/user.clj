27191628358e2e78b4e8a49fe3965bdca7abb0fc
zclj <info-s@tryne.se>
2022-02-10 14:59:51 +0100

    Add a skeleton fn to prep dev environment

--- project-references/standard-console/env/dev/clj/user.clj
+++ project-references/standard-console/env/dev/clj/user.clj
@@ -5,3 +5,12 @@
    [integrant.repl :refer [clear go halt prep init reset reset-all]]))
 
 (add-tap (bound-fn* clojure.pprint/pprint))
+
+(defn dev-prep!
+  "Preparing the dev environment consists of reading the system configration with the :dev profile and using this configuration to prepare integrant."
+  []
+  ;; set-prep! will store the system in integrant.repl.state/config
+  (integrant.repl/set-prep!
+   ;; set-prep! expects a zero arg fn that give the value of the
+   ;; integrant configuration to use
+   (fn [])))
