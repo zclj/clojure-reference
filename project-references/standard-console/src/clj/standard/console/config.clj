06a4b07dfa44b90297ec1eedb2e34b0507203807
zclj <info-s@tryne.se>
2022-02-10 14:13:28 +0100

    Add aero methods for integrant refs

--- project-references/standard-console/src/clj/standard/console/config.clj
+++ project-references/standard-console/src/clj/standard/console/config.clj
@@ -1,6 +1,15 @@
 (ns standard.console.config
-  (:require [aero.core]
+  (:require [aero.core :as aero]
             [clojure.java.io]
             [clojure.tools.logging :as log]
             [integrant.core :as ig]))
+
+(defmethod aero/reader 'ig/ref
+  [_ _ value]
+  (ig/ref value))
+
+(defmethod aero/reader 'ig/refset
+  [_ _ value]
+  (ig/refset value))
+
 
