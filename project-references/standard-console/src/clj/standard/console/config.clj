a66dbf243b97169fac54f75175cc8cd1e1aef81d
zclj <info-s@tryne.se>
2022-02-10 14:11:09 +0100

    Add required namespaces to config ns

--- project-references/standard-console/src/clj/standard/console/config.clj
+++ project-references/standard-console/src/clj/standard/console/config.clj
@@ -1,2 +1,6 @@
-(ns standard.console.config)
+(ns standard.console.config
+  (:require [aero.core]
+            [clojure.java.io]
+            [clojure.tools.logging :as log]
+            [integrant.core :as ig]))
 
